ec08161926880db7877888157b27198424de8cfc
pauloricardomg <pauloricardomg@gmail.com>
2014-05-21 00:09:04 -0300

    updated post tool to POST multiple movies

--- tools/post.sh
+++ tools/post.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
 
 if [[ $# != 1 ]]; then
   echo "Usage: $0 <movieJson>"
   exit 1
 fi
 
-FILENAME=$1
-
-curl -X POST -d @$FILENAME http://localhost:8080/movies --header "Content-Type:application/json"
+while read json
+do
+    curl -X POST -d"$json" http://localhost:8080/movies --header "Content-Type:application/json"
+    echo
+done < $1
